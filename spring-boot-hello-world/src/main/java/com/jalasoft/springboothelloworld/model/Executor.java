eec7c8305f85f6e2d08be4a6e07e2b5e4e114bde
Adriana Olivera <Adriana.Olivera@jala.university>
2022-10-12 15:06:10 -0400

    Add the Executor, Converter and VideoConverter classes

---
+++ spring-boot-hello-world/src/main/java/com/jalasoft/springboothelloworld/model/Executor.java
@@ -0,0 +1,41 @@
+package com.jalasoft.springboothelloworld.model;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Executor {
+    private Process process;
+
+    public void runCommand(List<String> command) {
+        initProcess(command);
+        //readConsole();
+    }
+
+    private void initProcess(List<String> command) {
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.redirectErrorStream(true);
+        try {
+            process = builder.start();
+        } catch (IOException e) {
+            System.out.println (e);
+        }
+    }
+    private void readConsole () {
+        try {
+            process.waitFor();
+            InputStreamReader streamReader = new InputStreamReader(process.getInputStream());
+            BufferedReader reader = new BufferedReader(streamReader);
+            StringBuilder result = new StringBuilder();
+            while(reader.ready()) {
+                result.append((char) reader.read());
+            }
+            System.out.println(result.toString());
+        } catch (InterruptedException e) {
+            System.out.println (e);
+        } catch (IOException e) {
+            System.out.println (e);
+        }
+    }
+}
