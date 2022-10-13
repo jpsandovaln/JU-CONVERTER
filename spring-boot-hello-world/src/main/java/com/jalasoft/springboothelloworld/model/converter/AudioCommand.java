4474ba133ee9581fae0830fde402bc64e4e2a4e5
Alvaro Sivila <alvaro.sivila@jala.university>
2022-10-13 19:25:41 -0400

    fixed the executor class and AudioCommand class

--- spring-boot-hello-world/src/main/java/com/jalasoft/springboothelloworld/model/converter/AudioCommand.java
+++ spring-boot-hello-world/src/main/java/com/jalasoft/springboothelloworld/model/converter/AudioCommand.java
@@ -1,64 +1,54 @@
 package com.jalasoft.springboothelloworld.model.converter;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class AudioConverter implements Converter {
+public class AudioCommand implements CommandBuilder {
 
     List <String> command = new ArrayList<>();
     String volume;
     String fileName;
     String exe;
     String format;
-    public AudioConverter(List<String> setup, String fileName) {
+
+    public AudioCommand(String fileName) {
         exe = ("ffmpeg\\bin\\ffmpeg.exe");
         this.fileName = fileName;
         command.add(exe);
         command.add("-i");
         command.add(fileName);
-        setParameters(setup, getName(fileName));
     }
-    public String getName(String name) {
+    private String getName(String name) {
         String[] parts = name.split("\\.");
         format = parts[1];
-        System.out.println(Arrays.asList(parts));
         return parts[0];
     }
-    public void selectEncode(String encode) {
+    private void selectEncode(String sampligFrecuency) {
 
-        switch (encode) {
-            case "aac_encode":
-                command.add("-c:a");
-                command.add("aac");
+        switch (sampligFrecuency) {
+            case "32000":
+                command.add("-ar");
+                command.add("32000");
                 break;
-            case "alac":
-                command.add("-c:a");
-                command.add("alac");
-                System.out.println("alac");
+            case "44100":
+                command.add("-ar");
+                command.add("44100");
                 break;
-            case "ac3":
-                command.add("-c:a");
-                command.add("ac3");
+            case "48000":
+                command.add("-ar");
+                command.add("48000");
                 break;
-            case "aac_mf":
-                command.add("-c:a");
-                command.add("aac_mf");
+            case "96000":
+                command.add("-ar");
+                command.add("96000");
         }
     }
-    public void setVolume(int volume) {
-        command.add("-vf");
-        if (volume > 0) {
-            command.add("volume=" + volume);
-        }
-    }
-    public void selectAudioChannel(String channels) {
+
+    private void selectAudioChannel(String channels) {
 
         switch (channels) {
             case "2":
                 command.add("-ac");
                 command.add("2");
-                System.out.println("chanels 2");
                 break;
             case "1":
                 command.add("-ac");
@@ -66,7 +56,7 @@ public class AudioConverter implements Converter {
                 break;
         }
     }
-    public void selectBitrateAudio(String bitrate) {
+    private void selectBitrateAudio(String bitrate) {
 
         switch (bitrate) {
             case "128k":
@@ -87,7 +77,7 @@ public class AudioConverter implements Converter {
                 break;
         }
     }
-    public void selectFormat(String format, String name) {
+    private void selectFormat(String format, String name) {
         switch (format) {
             case "mp3":
                 command.add(name + "1.mp3");
@@ -95,8 +85,11 @@ public class AudioConverter implements Converter {
             case "aac":
                 command.add(name + "1.aac");
                 break;
-            case "m4a":
-                command.add(name + "1.m4a");
+            case "ogg":
+                command.add(name + "1.ogg");
+                break;
+            case "flac":
+                command.add(name + "1.flac");
                 break;
             case "wma":
                 command.add(name + "1.wma");
@@ -106,30 +99,27 @@ public class AudioConverter implements Converter {
                 break;
         }
     }
-    public void setParameters(List<String> setup, String name) {
-        String value;
-        if (setup.contains(format)) {
 
-            for (int ind = 0; ind < setup.size(); ind++) {
-                value  = setup.get(ind);
+    @Override
+    public void setParameters(List<String> parameters) {
+        String value;
+        if (parameters.contains(format)) {
+
+            for (int ind = 0; ind < parameters.size(); ind++) {
+                value  = parameters.get(ind);
                 selectAudioChannel(value);
                 selectBitrateAudio(value);
-                selectFormat(value, name);
+                selectFormat(value, getName(fileName));
             }
         } else {
-            for (int ind = 0; ind < setup.size(); ind++) {
-                value  = setup.get(ind);
+            for (int ind = 0; ind < parameters.size(); ind++) {
+                value  = parameters.get(ind);
                 selectEncode(value);
                 selectAudioChannel(value);
                 selectBitrateAudio(value);
-                selectFormat(value, name);
+                selectFormat(value, getName(fileName));
             }
         }
-
-    }
-    @Override
-    public void setParameters(List<String> parameters) {
-
     }
     @Override
     public List<String> getCommand() {
