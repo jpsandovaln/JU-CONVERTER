0b62caeaeecf93e0fcf31bee57ecfb01146af848
FernandezRobert <soniic9911@gmail.com>
2022-10-20 19:56:24 -0400

    get the metadata of a file and return a text file

---
+++ spring-boot-hello-world/src/main/java/com/jalasoft/convert/model/MetadataExtractor.java
@@ -0,0 +1,23 @@
+package com.jalasoft.convert.model;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MetadataExtractor {
+    public static final String EXIFTOOL = "exiftool";
+    public static final String FILE_TXT = "file.txt";
+    private File file;
+    
+    public MetadataExtractor(File file) {
+        this.file = file;
+    }
+
+    public File extractMetadataTxt() throws IOException {
+        ProcessBuilder proc= new ProcessBuilder().command(EXIFTOOL, file.getPath());
+        proc.redirectErrorStream(true);
+        File f = new File(FILE_TXT);
+        proc.redirectOutput(f);
+        proc.start();
+        return f;
+    }
+}
