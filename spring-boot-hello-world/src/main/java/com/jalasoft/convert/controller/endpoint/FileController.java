616bbaed1eb4bf4629dfd932269405c58b118994
Mauricio Aliendre Perez <Maurcio.Aliendre@jala.university>
2022-10-27 16:47:19 -0400

    Added the whole middlewares for every controller

--- spring-boot-hello-world/src/main/java/com/jalasoft/convert/controller/endpoint/FileController.java
+++ spring-boot-hello-world/src/main/java/com/jalasoft/convert/controller/endpoint/FileController.java
@@ -8,8 +8,8 @@
  */
 package com.jalasoft.convert.controller.endpoint;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jalasoft.convert.common.logger.At18Logger;
+import com.jalasoft.convert.controller.response.UploadFileResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -20,10 +20,10 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jalasoft.convert.controller.service.FileStorageService;
-import com.jalasoft.convert.controller.response.UploadFileResponse;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 /**
  * It is responsable for uploading and downloading all type of files 
@@ -35,7 +35,7 @@ import java.io.IOException;
 @RestController
 public class FileController {
 
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+    private static final Logger LOG = new At18Logger().getLogger();
 
     @Autowired
     private FileStorageService fileStorageService;
@@ -62,7 +62,7 @@ public class FileController {
         try {
             contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
         } catch (IOException ex) {
-            logger.info("Could not determine file type.");
+            LOG.info("Could not determine file type.");
         }
 
         // If content type if type could not be determined
