8d9f01309459df3bbfd5186a2dabe1faa7b76618
Hugo Solares <Hugo.Solares@jala.university>
2022-10-29 17:18:53 -0400

    adding logger functionalities to all controllers

--- spring-boot-hello-world/src/main/java/com/jalasoft/convert/controller/endpoint/DocController.java
+++ spring-boot-hello-world/src/main/java/com/jalasoft/convert/controller/endpoint/DocController.java
@@ -1,10 +1,11 @@
 package com.jalasoft.convert.controller.endpoint;
 
 import com.jalasoft.convert.common.exception.ConverterFileException;
+import com.jalasoft.convert.common.logger.At18Logger;
 import com.jalasoft.convert.controller.response.ErrorResponse;
 import com.jalasoft.convert.controller.response.Response;
-import com.jalasoft.convert.controller.service.FileStorageService;
 import com.jalasoft.convert.controller.service.ConvertWordDocumentToPDF;
+import com.jalasoft.convert.controller.service.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
+import java.util.logging.Logger;
 
 /**
  * It is responsable for uploading Word document created with MS Office (docx)
@@ -26,15 +28,19 @@ import java.nio.file.Path;
  */
 @RestController
 public class DocController {
+    private static final Logger LOG = new At18Logger().getLogger();
     @Autowired
     private FileStorageService fileStorageService;
     @Autowired
     private ConvertWordDocumentToPDF documentWord;
 
     @PostMapping("/convertdocument")
-    public Response readDoc(@RequestParam("file") MultipartFile file, HttpServletResponse response){
+
+    public Response readDoc(@RequestParam("file") MultipartFile file, HttpServletResponse response) {
         try {
             Path pathFile = fileStorageService.save(file);
+            String fileName = pathFile.toString();
+            LOG.info("File uploaded: " + fileName);
             InputStream inputStream = new FileInputStream(pathFile.toFile());
             documentWord.convertWordDocument(inputStream, response.getOutputStream());
             response.addHeader("Content-disposition", "attachment; filename=" + "doc.pdf");
@@ -42,8 +48,7 @@ public class DocController {
             response.flushBuffer();
             return new Response("200");
         } catch (ConverterFileException | IOException e) {
-            return new ErrorResponse("400",e.getMessage());
-
+            return new ErrorResponse("400", e.getMessage());
         }
     }
 }
