1af072781a8ea3628e5ed128ba9be8b012de7646
Libertad Tolaba <Libertad.Tolaba@jala.university>
2022-10-31 11:22:08 -0400

    Added documentation for exceptions

--- spring-boot-hello-world/src/main/java/com/jalasoft/convert/common/exception/TesseractException.java
+++ spring-boot-hello-world/src/main/java/com/jalasoft/convert/common/exception/TesseractException.java
@@ -1,5 +1,18 @@
+/**
+ * Copyright (c) 2022 Jala University.
+ *
+ * This software is the confidential and proprieraty information of Jalasoft
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * Licence agreement you entered into with Jalasoft
+ */
 package com.jalasoft.convert.common.exception;
-
+/**
+ * It takes care of the exception
+ *
+ * @author Libertad Tolaba
+ * @version 1.0
+ */
 public class TesseractException extends Exception{
     public TesseractException(String message) {
         super(message);
